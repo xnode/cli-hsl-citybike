9501a23ab455d31573febad922e7f2ba1aba0b8b
Toni Ahola <tkahola@gmail.com>
2016-07-01 18:12:22 +0300

    basic tests for getStation

--- spec/hsl-bike_spec.js
+++ spec/hsl-bike_spec.js
@@ -1,9 +1,82 @@
 'use strict';
 
+var MockAdapter = require('axios-mock-adapter');
+var hslBike = require('../lib/hsl-bike');
+
 describe('get Stations', () => {
+    var mock = new MockAdapter(hslBike.testInstance);
+
+    beforeAll(() => {
+        mock.onGet('http://api.digitransit.fi/routing/v1/routers/hsl/bike_rental').reply(200, {
+            stations: [
+                {
+                    id: 'T1',
+                    name: 'Test Place',
+                    x: 1.968021,
+                    y: 1.1648,
+                    bikesAvailable: 3,
+                    spacesAvailable: 13,
+                    allowDropoff: true,
+                    realTimeData: true
+                },
+                {
+                    id: 'T2',
+                    name: 'Test Place2',
+                    x: 1.975561,
+                    y: 2.167891,
+                    bikesAvailable: 0,
+                    spacesAvailable: 12,
+                    allowDropoff: true,
+                    realTimeData: true
+                }
+            ]
+        });
+    });
+
+    afterAll(() => {
+        mock.restore();
+    });
+
     describe('by location', () => {
         it('contains total spaces', () => {
 
+        });
+    });
+
+    describe('by station', () => {
+        var response = {};
+
+        beforeEach((done) => {
+            response = {};
+            hslBike.getByStation('Test Place', 5)
+                .then((res) => {
+                    response = res;
+                    done();
+                })
+                .catch((error) => {
+                    done.fail(error);
+                });
+        });
+
+        it('expect name to be Test Place', () => {
+            var station = response[0];
+            expect(station.name).toEqual('Test Place');
+        });
+
+        it('expect to contain total spaces', () => {
+            var station = response[0];
+            expect(station.totalSpaces).toEqual(16);
+        });
+
+        it('expect to have distance 0', () => {
+            var station = response[0];
+            expect(station.distance).toEqual(0);
+        });
+
+        it('expect to have latitude and longitude', () => {
+            var station = response[0];
+            expect(station.latitude).toBeDefined();
+            expect(station.longitude).toBeDefined();
         });
     });
 });
